a3f8a4fa0e92d8fd7534fcbf78f7e98756b2eff8
riverofrancisco <francisco.rivero124@gmail.com>
2022-11-06 17:38:50 -0300

    Advanced Promises Chapter 1, 2 & 3

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -2,8 +2,63 @@
 /*----------------------------------------------------------------
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
-// // TU CÓDIGO AQUÍ:
+// // TU CÓDIGO AQUÍ://arg == function arg (resolve,reject)=>{blablabla}
+function $Promise(executor){
+if(typeof executor !== 'function') throw TypeError('executor function');
 
+this._state = 'pending';
+this._internalResolve = (data)=>{
+    if(this._state === 'pending'){
+        this._state = 'fulfilled';
+        this._value = data;}
+    this.callHandlers();
+};
+this._internalReject = (data)=>{
+    if(this._state === 'pending'){
+        this._state = 'rejected';
+        this._value = data;}
+    this.callHandlers();
+};
+
+this._handlerGroups = [];
+
+this.then = (sH, eH) => {
+    if (typeof sH !== 'function') sH = false;
+    if (typeof eH !== 'function') eH = false; 
+    this._handlerGroups.push({successCb: sH, errorCb: eH});
+    if (this._state !== 'pending') this.callHandlers();
+}
+
+executor(this._internalResolve, this._internalReject);
+
+
+
+}
+     
+$Promise.prototype.callHandlers = function(){
+    while(this._handlerGroups.length > 0){
+        let current = this._handlerGroups.shift() //tengo mi primer elemento.
+        if(this._state === 'fulfilled'){
+            current.successCb && current.successCb(this._value);
+        } else if (this._state === 'rejected') {
+            current.errorCb && current.errorCb(this._value);
+        }
+/*         if(this._state !== 'pending'){
+            if(this._state === 'fulfilled') {
+                if(this._handlerGroups.length > 0 && this._handlerGroups[0].successCb !== false)
+                executor(this._handlerGroups[0].successCb)
+            }
+            if(this._state === 'rejected') {
+                if(this._handlerGroups.length > 0 && this._handlerGroups[0].errorCb !== false)
+                executor(this._handlerGroups[0].errorCb)
+            }
+        } */
+    }
+}
+
+$Promise.prototype.catch = function(errorCb){
+    return this.then(null, errorCb);
+}
 
 
 module.exports = $Promise;
