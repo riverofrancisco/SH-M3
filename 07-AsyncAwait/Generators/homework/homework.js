2048326ba1c0f345c9977e47b7dcffa5cb25355b
riverofrancisco <francisco.rivero124@gmail.com>
2022-11-15 19:00:49 -0300

    Generators commit

--- 07-AsyncAwait/Generators/homework/homework.js
+++ 07-AsyncAwait/Generators/homework/homework.js
@@ -1,6 +1,34 @@
-function fizzBuzzGenerator(max) {
+function* fizzBuzzGenerator(max) {
   // Tu código acá:
+  let number = 1;
+  while(number <= max || !max) {
+        let returnedMessage = [];
+        
+        if(number % 3 !== 0 && number % 5 !== 0) yield number;
+        
+        if(number % 3 === 0){
+          returnedMessage.push('Fizz')}
+        
+        if(number % 5 === 0){
+          returnedMessage.push('Buzz')}
+        
+        if(returnedMessage.length > 0){
+          yield returnedMessage.join(' ')
+        };
+        
+        number = number + 1;
+  }
 
 }
+let a = fizzBuzzGenerator(100)
+
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
+console.log(a.next())
 
 module.exports = fizzBuzzGenerator;
